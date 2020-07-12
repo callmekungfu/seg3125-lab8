d6ac8800586a104d2fb99bd9363c99b02aa9c1cd
Yong Lin <wangyonglin1999@gmail.com>
2020-07-12 13:23:43 -0400

    feat: add a basic shop front

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
 import './App.css';
+import ShopComponent from './components/Shop';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Rental Store</Menu.Item>
+          <Menu.Item key="2">Your Order</Menu.Item>
+          <Menu.Item key="3">Contact Us</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          <Router>
+            <Switch>
+              <Route path="/">
+                <ShopComponent />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Crafted by Yong Lin Wang</Footer>
+    </Layout>
   );
 }
 
